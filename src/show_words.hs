3e009064d24239b127c488e62730ca442a9aaec8
sgf <sgf.dma@gmail.com>
2012-08-20 20:41:53 +0400

    Rewrite list indexing. Rewrite remove surrounding spaces.
        - Rewrite folding funtions for both wordByInd (renamed to phraseByInd) and
          wordByNotInd (renamed to phraseByNotInd). Now folding functions increase
          state at each element. I.e. there is no mean of separator and list
          elements with equal state now. This change is requird to implement
          indByPhrase. Also, it's better to split code to separate line into
          phrases (what was previously sequence of list elements with equal state)
          and code to access list elements by index.
        - Add indByPhrase.
        - Prepare replacement for rmSurrSpaces: there is much simplier solution
          then State and Backward State monads in a row - just use dropWhile and
          dropWhileEnd.
        - Add splitToPhrases to split line by separators and remove surrounding
          spaces.
        - Clean up in next commit.

--- src/show_words.hs
+++ src/show_words.hs
@@ -1,7 +1,10 @@
 
 import System.IO                -- For hX
 import System.Environment       -- For getArgs
-import Control.Monad            -- ?
+import Data.List                -- For groupBy
+import Data.Char                -- For isSpace
+import Data.Maybe               -- For fromJust
+import Control.Monad
 import Control.Monad.State      -- For State monad.
 
 type Index          =  Int
@@ -22,6 +25,21 @@ foldlM              :: (Monad m) => (b -> a -> m b) -> b -> [a] -> m b
 foldlM g z []       =  return z
 foldlM g z (x : xs) =  g z x >>= \z' -> foldlM g z' xs
 
+-- Data.List does not contain it on my system.
+dropWhileEnd :: (a -> Bool) -> [a] -> [a]
+dropWhileEnd p      = foldr (\x z -> if null z && p x then [] else x : z) []
+
+-- Split list by separator (omitting separator itself).
+splitBy :: (Eq a) => a -> [a] -> [[a]]
+splitBy s           = foldr go [[]]
+  where
+    go x zl@(z : zs) = if x == s then [] : zl else (x : z) : zs
+
+-- FIXME: Use isSapce instead of check for ' '.
+-- FIXME: Use dropWhile and dropWhileEnd instead of rmSurrSpaces.
+-- FIXME: First split list by separator (omitting separator), then parse it
+-- with State monad. I.e. group elements at the level lower, then State monads
+-- works at.
 main                =  do
     (file : args) <- getArgs
     handle <- openFile file ReadMode
@@ -29,39 +47,89 @@ main                =  do
     sequence $ map putStrLn $ lines contents
 --getContents >>= sequence . map putStrLn . lines
 
---fieldNameToNum
-seqElems :: String -> [String]
-seqElems ls         =  map (wordByInd ls) [2,1,3]
-
-    --map (elemByInd ls) transp
-
+-- Phrase separator.
+phrSep              = '-'
 -- Start index.
 indBase             = 1
--- Word separator.
-wordSep             = '-'
+
+-- Split each string to phrases by phrSep character (omitting separator
+-- itself) and remove leading and trailing spaces from each phrase (phrase may
+-- contain spaces).
+splitToPhrases :: [String] -> [[String]]
+splitToPhrases      = map splitToPhrases'
+  where
+    splitToPhrases' :: String -> [String]
+    splitToPhrases' = map dropSpaces . splitBy phrSep
+    dropSpaces :: String -> String
+    dropSpaces      = dropWhile isSpace . dropWhileEnd isSpace
+
+-- Indexed lists {{{
+
+-- Choose phrase from list by index.
+phraseByInd :: [String] -> Index -> [String]
+phraseByInd ls i    = fst $ runBState phraseByInd' indBase
+  where
+    phraseByInd' :: BState Index [String]
+    phraseByInd'    = foldrM (\x -> BState . onlyInd i x) [] ls
+
+-- Complement phraseByInd. Choose all phrases from list with index not equal
+-- to specified one.
+phraseByNotInd :: [String] -> Index -> [String]
+phraseByNotInd ls i = fst $ runBState phraseByNotInd' indBase
+  where
+    phraseByNotInd' :: BState Index [String]
+    phraseByNotInd' = foldrM (\x -> BState . notInd i x) [] ls
+
+-- Reverse of phraseByInd. Determine index by phrase.
+indByPhrase :: [String] -> String -> Maybe Index
+indByPhrase ls y    = fst $ runBState indByPhrase' indBase
+  where
+    indByPhrase' :: BState Index (Maybe Index)
+    indByPhrase'    = foldrM (\x z -> BState $ indOf y x z) Nothing ls
+
 
 -- Choose word from string by index.
-wordByInd :: String -> Index -> String
-wordByInd ls i      = fst $ runBState wordByInd' indBase
+phraseByInd1 :: String -> Index -> String
+phraseByInd1 ls i   = fst $ runBState phraseByInd1' indBase
   where
-    wordByInd' :: BState Index String
-    wordByInd'      = foldrM (\x -> BState . onlyInd wordSep i x) "" ls
+    phraseByInd1' :: BState Index String
+    phraseByInd1'   = foldrM (\x -> BState . onlyInd1 phrSep i x) "" ls
 
 -- Choose all other words from string with index not equal to specified one.
-wordByNotInd :: String -> Index -> String
-wordByNotInd ls i   = fst $ runBState wordByNotInd' indBase
+phraseByNotInd1 :: String -> Index -> String
+phraseByNotInd1 ls i = fst $ runBState phraseByNotInd1' indBase
   where
-    wordByNotInd' :: BState Index String
-    wordByNotInd'   = foldrM (\x -> BState . notInd wordSep i x) "" ls
+    phraseByNotInd1' :: BState Index String
+    phraseByNotInd1' = foldrM (\x -> BState . notInd1 phrSep i x) "" ls
 
--- Several folding functions for use in State or Backward State monads. {{{
+-- END Indexed lists }}}
+-- Folding functions for use in State or Backward State monads. {{{
+
+-- Add list element x to the accumulator z only if its index s equals to i.
+onlyInd :: Index -> a -> [a] -> Index -> ([a], Index)
+onlyInd i x z       = \s -> let z' = if s == i then x : z else z
+                            in  (z', s + 1)
+
+-- Complement to onlyInd. Add each list elements x to accumulator z if its
+-- index s not equals to i.
+notInd :: Index -> a -> [a] -> Index -> ([a], Index)
+notInd i x z        = \s -> let z' = if s /= i then x : z else z
+                            in  (z', s + 1)
+
+-- Reverse of onlyInd. Set accumulator to index of list element y. If there is
+-- several, first one (closer to the list head) will be used.
+indOf :: (Eq a) => a -> a -> Maybe Index -> Index -> (Maybe Index, Index)
+indOf y x z         = \s -> let z' = if x == y then Just s else z
+                            in  (z', s + 1)
+
 
 -- Add element x to the accumulator z only if elememt's index s equals to i.
 -- Elements are separated by 'sep'.
-onlyInd :: (Eq a) => a -> Index -> a -> [a] -> Index -> ([a], Index)
-onlyInd sep i x z   = \s -> if x == sep
+onlyInd1 :: (Eq a) => a -> Index -> a -> [a] -> Index -> ([a], Index)
+onlyInd1 sep i x z  = \s -> if x == sep
                               then (z, s + 1)
                               else if s == i then (x : z, s) else (z, s)
+
 -- Add element x to the accumulator z only if element's index s does _not_
 -- equal to i. Elements are seprated by sep. Several elements in the result
 -- is possible.
@@ -71,8 +139,8 @@ onlyInd sep i x z   = \s -> if x == sep
 -- I should decide to add separator by checking previous state, but not next
 -- one. Otherwise, check 'not (null z)' will be always true (it will not with
 -- left fold, but left fold reverses list).
-notInd :: (Eq a) => a -> Index -> a -> [a] -> Index -> ([a], Index)
-notInd sep i x z    = \s ->
+notInd1 :: (Eq a) => a -> Index -> a -> [a] -> Index -> ([a], Index)
+notInd1 sep i x z   = \s ->
                         if x == sep
                           then let s' = s + 1
                                    z' = if s /= i && not (null z)
