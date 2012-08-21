b6d98b269fae39f999a03d7808f8fa5c7a6c63a0
sgf <sgf.dma@gmail.com>
2012-08-21 15:40:08 +0400

    Make list index functions work with list of indexes.
        - Folding functions now check match using `elem` for list if indexes (or
          elements for indsByElems).
        - List index functions versions for list of indexes: elemsByInds,
          elemsByNotInds, indsByElems.
        - I just change folding functions. No monoids, no nested monads (Reader,
          Maybe, List, etc). Well, this is change is really much more simplier,
          than map existing single-index folding functions to list of indexes,
          then unwrap results, mappend them, wrap again in monad and preserve
          proper state change context along with all this mess. Well, it may be
          done, it will not be neither nice, nor good. Maybe, because i don't use
          monad transformers.
        - In next commit: Cleanup and rewrite of Phrase functions to use new
          versions of list index functions

--- src/show_words.hs
+++ src/show_words.hs
@@ -4,9 +4,11 @@ import System.Environment       -- For getArgs
 import Data.List                -- For groupBy
 import Data.Char                -- For isSpace
 import Data.Maybe               -- For fromJust
+import Data.Monoid
 import Control.Monad
 import Control.Applicative
 import Control.Monad.State      -- For State monad.
+import Control.Monad.Reader
 
 type Index          =  Int
 newtype BState s a  =  BState {runBState :: (s -> (a, s))}
@@ -43,11 +45,17 @@ splitBy s           = foldr go [[]]
 onlyInd :: Index -> a -> Maybe a -> Index -> (Maybe a, Index)
 onlyInd i x z       = \s -> let z' = if s == i then Just x else z
                             in  (z', s + 1)
+onlyInds :: [Index] -> a -> [a] -> Index -> ([a], Index)
+onlyInds js x z     = \s -> let z' = if s `elem` js then x : z else z
+                            in  (z', s + 1)
 
 -- Complement to onlyInd. Add each list elements x to accumulator z if its
 -- index s not equals to i.
 notInd :: Index -> a -> [a] -> Index -> ([a], Index)
 notInd i x z        = \s -> let z' = if s /= i then x : z else z
+                            in  (z', s + 1)
+notInds :: [Index] -> a -> [a] -> Index -> ([a], Index)
+notInds js x z      = \s -> let z' = if s `notElem` js then x : z else z
                             in  (z', s + 1)
 
 -- Reverse of onlyInd. Set accumulator to index of list element y. If there is
@@ -55,16 +63,53 @@ notInd i x z        = \s -> let z' = if s /= i then x : z else z
 indsOf :: (Eq a) => a -> a -> [Index] -> Index -> ([Index], Index)
 indsOf y x z         = \s -> let z' = if x == y then s : z else z
                             in  (z', s + 1)
+indsOfs :: (Eq a) => [a] -> a -> [Index] -> Index -> ([Index], Index)
+indsOfs ys x z      = \s -> let z' = if x `elem` ys then s : z else z
+                            in  (z', s + 1)
+
 
 -- Index list by folding inside Backward State monads.
 elemByInd :: Index -> [a] -> BState Index (Maybe a)
 elemByInd i         = foldrM (\x -> BState . onlyInd i x) Nothing
+elemsByInds :: [Index] -> [a] -> BState Index [a]
+elemsByInds js      = foldrM (\x -> BState . onlyInds js x) []
 
 elemsByNotInd :: Index -> [a] -> BState Index [a]
 elemsByNotInd i     = foldrM (\x -> BState . notInd i x) []
+elemsByNotInds :: [Index] -> [a] -> BState Index [a]
+elemsByNotInds js   = foldrM (\x -> BState . notInds js x) []
 
 indsByElem :: (Eq a) => a -> [a] -> BState Index [Index]
 indsByElem y        = foldrM (\x -> BState . indsOf y x) []
+indsByElems :: (Eq a) => [a] -> [a] -> BState Index [Index]
+indsByElems ys      = foldrM (\x -> BState . indsOfs ys x) []
+
+-- *Main> runReader (liftM2 pp (Reader (\s -> ([1,2], s + 1))) (Reader (\s -> ([3,4], s + 1)))) 2
+-- ([1,2,3,4],3)
+-- *Main> runReader ((sequence $ [Reader (\s -> ([1,2], s + 1)), Reader (\s -> ([3,4], s + 1))]) >>= \xs -> return $ foldr  (\(xs, s) (zs, _) -> (xs ++ zs, s)) ([], 0) xs) 1
+-- ([1,2,3,4],2)
+-- Looks promising, doesn't it?
+pp :: ([a], Index) -> ([a], Index) -> ([a], Index)
+pp (xs, s) (ys, _)  = (xs ++ ys, s)
+
+-- Versions for list of indexes.
+-- FIXME: The problem with reusing exisintg notInd is, that notInd outputs
+-- resulting accumulator for every index, and there is no way then to
+-- disassemble it and determine whether current x was added or not. It should
+-- output not resulting accumulator, but monoid. Then i can use `mappend` here
+-- to get accumulator. Version with monoid should suit both single index and
+-- multiple index versions.
+-- Umm.. well, or maybe monad? Maybe monad?
+-- Result of notInd should in Maybe monad. Then all result must be binded, so
+-- Nothing will zeroing all other values. Then resulting Maybe value should be
+-- added to the list: if Nothing, then Nothing added, otherwise unwrap Just
+-- and add value. Functions from Data.Maybe, with default value, should do
+-- this well.
+-- 1. For each j notInd -> [Just x, Nothing, Just x]
+-- 2. fold it into Maybe monad.
+-- 3. Add result to accumulator using `maybe z (: z)` or fromMaybe.
+{-elemByNotInds :: Index -> [a] -> BState Index [a]
+elemByNotInds js    = foldrM (\x z -> BState $ notInd i x z) []-}
 
 -- END Indexed list. }}}
 
@@ -97,15 +142,14 @@ splitToPhrases      = map splitToPhrases'
     dropSpaces :: String -> String
     dropSpaces      = dropWhile isSpace . dropWhileEnd isSpace
 
-{-
-createOrder :: [String] -> [String] -> [Maybe Index]
-createOrder refs    = map (indByPhrase refs)-}
+-- Create list of phrase column numbers (indexes) from list of column names.
+createOrder :: [String] -> [String] -> [Index]
+createOrder refs xs = xs >>= indsByPhrase refs
+--createOrder refs    = concat . map (indsByPhrase refs)
 
--- FIXME: Use isSapce instead of check for ' '.
--- FIXME: Use dropWhile and dropWhileEnd instead of rmSurrSpaces.
--- FIXME: First split list by separator (omitting separator), then parse it
--- with State monad. I.e. group elements at the level lower, then State monads
--- works at.
+reorderPhrases :: [[String]] -> [[String]]
+reorderPhrases = undefined
+
 main                =  do
     (file : args) <- getArgs
     handle <- openFile file ReadMode
