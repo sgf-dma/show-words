7e1a672d57c8457af3848f940bbdd5c358e6a688
sgf <sgf.dma@gmail.com>
2012-08-21 19:56:44 +0400

    Reorder phrases. Outdated code cleanup.
        - Remove single-index folding functions from list indexing implementation.
        - Outdated code clean up.
        - Write reorderPhrases function for order Phrases in each line according
          to specified on cmd order.

--- src/show_words.hs
+++ src/show_words.hs
@@ -42,74 +42,34 @@ splitBy s           = foldr go [[]]
 
 -- Folding functions for use in State or Backward State monads.
 -- Add list element x to the accumulator z only if its index s equals to i.
-onlyInd :: Index -> a -> Maybe a -> Index -> (Maybe a, Index)
-onlyInd i x z       = \s -> let z' = if s == i then Just x else z
-                            in  (z', s + 1)
 onlyInds :: [Index] -> a -> [a] -> Index -> ([a], Index)
 onlyInds js x z     = \s -> let z' = if s `elem` js then x : z else z
                             in  (z', s + 1)
 
 -- Complement to onlyInd. Add each list elements x to accumulator z if its
 -- index s not equals to i.
-notInd :: Index -> a -> [a] -> Index -> ([a], Index)
-notInd i x z        = \s -> let z' = if s /= i then x : z else z
-                            in  (z', s + 1)
 notInds :: [Index] -> a -> [a] -> Index -> ([a], Index)
 notInds js x z      = \s -> let z' = if s `notElem` js then x : z else z
                             in  (z', s + 1)
 
+-- FIXME: Supply own equality test. This allows to remove (Eq a) restriction
+-- and allow to use some prefix matching instead of `elem`.
 -- Reverse of onlyInd. Set accumulator to index of list element y. If there is
 -- several, first one (closer to the list head) will be used.
-indsOf :: (Eq a) => a -> a -> [Index] -> Index -> ([Index], Index)
-indsOf y x z         = \s -> let z' = if x == y then s : z else z
-                            in  (z', s + 1)
 indsOfs :: (Eq a) => [a] -> a -> [Index] -> Index -> ([Index], Index)
 indsOfs ys x z      = \s -> let z' = if x `elem` ys then s : z else z
                             in  (z', s + 1)
 
 
--- Index list by folding inside Backward State monads.
-elemByInd :: Index -> [a] -> BState Index (Maybe a)
-elemByInd i         = foldrM (\x -> BState . onlyInd i x) Nothing
+-- Index list by folding inside Backward State monad.
 elemsByInds :: [Index] -> [a] -> BState Index [a]
 elemsByInds js      = foldrM (\x -> BState . onlyInds js x) []
 
-elemsByNotInd :: Index -> [a] -> BState Index [a]
-elemsByNotInd i     = foldrM (\x -> BState . notInd i x) []
 elemsByNotInds :: [Index] -> [a] -> BState Index [a]
 elemsByNotInds js   = foldrM (\x -> BState . notInds js x) []
 
-indsByElem :: (Eq a) => a -> [a] -> BState Index [Index]
-indsByElem y        = foldrM (\x -> BState . indsOf y x) []
 indsByElems :: (Eq a) => [a] -> [a] -> BState Index [Index]
 indsByElems ys      = foldrM (\x -> BState . indsOfs ys x) []
-
--- *Main> runReader (liftM2 pp (Reader (\s -> ([1,2], s + 1))) (Reader (\s -> ([3,4], s + 1)))) 2
--- ([1,2,3,4],3)
--- *Main> runReader ((sequence $ [Reader (\s -> ([1,2], s + 1)), Reader (\s -> ([3,4], s + 1))]) >>= \xs -> return $ foldr  (\(xs, s) (zs, _) -> (xs ++ zs, s)) ([], 0) xs) 1
--- ([1,2,3,4],2)
--- Looks promising, doesn't it?
-pp :: ([a], Index) -> ([a], Index) -> ([a], Index)
-pp (xs, s) (ys, _)  = (xs ++ ys, s)
-
--- Versions for list of indexes.
--- FIXME: The problem with reusing exisintg notInd is, that notInd outputs
--- resulting accumulator for every index, and there is no way then to
--- disassemble it and determine whether current x was added or not. It should
--- output not resulting accumulator, but monoid. Then i can use `mappend` here
--- to get accumulator. Version with monoid should suit both single index and
--- multiple index versions.
--- Umm.. well, or maybe monad? Maybe monad?
--- Result of notInd should in Maybe monad. Then all result must be binded, so
--- Nothing will zeroing all other values. Then resulting Maybe value should be
--- added to the list: if Nothing, then Nothing added, otherwise unwrap Just
--- and add value. Functions from Data.Maybe, with default value, should do
--- this well.
--- 1. For each j notInd -> [Just x, Nothing, Just x]
--- 2. fold it into Maybe monad.
--- 3. Add result to accumulator using `maybe z (: z)` or fromMaybe.
-{-elemByNotInds :: Index -> [a] -> BState Index [a]
-elemByNotInds js    = foldrM (\x z -> BState $ notInd i x z) []-}
 
 -- END Indexed list. }}}
 
@@ -119,41 +79,49 @@ phrSep              = '-'
 indBase             = 1
 
 -- Choose phrase from list by index.
-phraseByInd :: [String] -> Index -> Maybe String
-phraseByInd ls i    = fst $ runBState (elemByInd i ls) indBase
+phraseByInd :: [String] -> Index -> [String]
+phraseByInd ls i    = fst $ runBState (elemsByInds [i] ls) indBase
 
 -- Complement phraseByInd. Choose all phrases from list with index not equal
 -- to specified one.
 phrasesByNotInd :: [String] -> Index -> [String]
-phrasesByNotInd ls i = fst $ runBState (elemsByNotInd i ls) indBase
+phrasesByNotInd ls i = fst $ runBState (elemsByNotInds [i] ls) indBase
 
 -- Reverse of phraseByInd. Determine index by phrase.
 indsByPhrase :: [String] -> String -> [Index]
-indsByPhrase ls y   = fst $ runBState (indsByElem y ls) indBase
+indsByPhrase ls y   = fst $ runBState (indsByElems [y] ls) indBase
 
 -- Split each string to phrases by phrSep character (omitting separator
 -- itself) and remove leading and trailing spaces from each phrase (phrase may
 -- contain spaces).
-splitToPhrases :: [String] -> [[String]]
-splitToPhrases      = map splitToPhrases'
+splitToPhrases :: String -> [String]
+splitToPhrases      = map dropSpaces . splitBy phrSep
   where
-    splitToPhrases' :: String -> [String]
-    splitToPhrases' = map dropSpaces . splitBy phrSep
     dropSpaces :: String -> String
     dropSpaces      = dropWhile isSpace . dropWhileEnd isSpace
 
+{-
 -- Create list of phrase column numbers (indexes) from list of column names.
 createOrder :: [String] -> [String] -> [Index]
-createOrder refs xs = xs >>= indsByPhrase refs
---createOrder refs    = concat . map (indsByPhrase refs)
+createOrder xs refs = xs >>= indsByPhrase refs
+--createOrder refs    = concat . map (indsByPhrase refs)-}
 
-reorderPhrases :: [[String]] -> [[String]]
-reorderPhrases = undefined
+reorderPhrases :: [String] -> [String] -> [[String]]
+reorderPhrases colNames (refs : ls)
+                    = map (orderPhrases . splitToPhrases) ls
+  where
+    phrOrder        :: [Index]
+    phrOrder        = colNames >>= indsByPhrase refs'
+    --phrOrder        = concat $ map (indsByPhrase refs') colNames
+      where refs'   = splitToPhrases refs
+    orderPhrases :: [String] -> [String]
+    orderPhrases ls = phrOrder >>= phraseByInd ls
+    --orderPhrases ls = concat $ map (phraseByInd ls) phrOrder
 
 main                =  do
     (file : args) <- getArgs
     handle <- openFile file ReadMode
     contents <- hGetContents handle
-    sequence $ map putStrLn $ lines contents
---getContents >>= sequence . map putStrLn . lines
+    sequence $ map putStrLn $ concat $ reorderPhrases args $ lines contents
+    hClose handle
 
