cb1e07b505396142088ca84374d1491adf8846ed
sgf <sgf.dma@gmail.com>
2012-09-13 16:47:36 +0400

    Revert Foldable into list again.
    Really, there is absoultely no need in this additional dependency, so revert
    it. And there is time to merge in master, i think.. ^)

--- src/SgfList.hs
+++ src/SgfList.hs
@@ -18,27 +18,25 @@ module SgfList
     )
   where
 
-import qualified Data.Foldable as F
+import Data.Monoid
 import Control.Applicative
 import Control.Monad.State
 import Control.Monad.Reader
 import System.Random (RandomGen, randomRs)
-import Data.Monoid
 
 -- Reimplement list indexing part of Data.List using Backward State monad. And
 -- some more useful functions for lists using State monads.
-
 
 foldrM :: (Monad m) => (a -> b -> m b) -> b -> [a] -> m b
 foldrM _ z []         = return z
 foldrM g z (x : xs)   = foldrM g z xs >>= g x
 {-
-foldrM :: (F.Foldable t, Monad m) => (a -> b -> m b) -> b -> t a -> m b
-foldrM g z xs       = F.foldr (\x mz -> mz >>= g x) (return z) xs-}
-{-
-foldlM :: (F.Foldable t, Monad m) => (b -> a -> m b) -> b -> t a -> m b
+foldrM :: (Foldable t, Monad m) => (a -> b -> m b) -> b -> t a -> m b
+foldrM g z xs       = foldr (\x mz -> mz >>= g x) (return z) xs-}
+foldlM :: (Monad m) => (b -> a -> m b) -> b -> [a] -> m b
 foldlM _ z []         = return z
-foldlM g z (x : xs)   = g z x >>= \z' -> foldlM g z' xs-}
+foldlM g z (x : xs)   = g z x >>= \z' -> foldlM g z' xs
+
 -- Backward state monad from "The essence of functional programming" by Philip
 -- Wadler.
 newtype BState s a  = BState {runBState :: (s -> (a, s))}
@@ -77,42 +75,39 @@ onlyElems p x z s
 -- Index list by right folding it inside Backward State monad.
 --
 -- Find all elements with specified indexes.
-elemsByIndsM :: (F.Foldable t) => [Index] -> t a -> BState Index [a]
-elemsByIndsM js     = F.foldrM (\x -> BState . onlyInds (`elem` js) x) []
+elemsByIndsM :: [Index] -> [a] -> BState Index [a]
+elemsByIndsM js     = foldrM (\x -> BState . onlyInds (`elem` js) x) []
 
 -- Complement to elemsByInds. Find all elements with _not_ specified indexes.
-elemsByNotIndsM :: (F.Foldable t) => [Index] -> t a -> BState Index [a]
-elemsByNotIndsM js  = F.foldrM (\x -> BState . onlyInds (`notElem` js) x) []
+elemsByNotIndsM :: [Index] -> [a] -> BState Index [a]
+elemsByNotIndsM js  = foldrM (\x -> BState . onlyInds (`notElem` js) x) []
 
 -- Reverse of elemsByInds. Find all indexes of specified elements.
-indsByElemsM :: (F.Foldable t, F.Foldable t1) =>
-                (a -> a -> Bool) -> t1 a -> t a -> BState Index [Index]
-indsByElemsM eq ks  = F.foldrM (\x -> BState . onlyElems p x) []
-  where p x         = F.any (`eq` x) ks
+indsByElemsM :: (a -> a -> Bool) -> [a] -> [a] -> BState Index [Index]
+indsByElemsM eq ks  = foldrM (\x -> BState . onlyElems p x) []
+  where p x         = any (`eq` x) ks
 
 -- Unwrap monad from list indexing functions.
-elemsByInds :: (F.Foldable t) => t a -> [Index] -> [a]
+elemsByInds :: [a] -> [Index] -> [a]
 elemsByInds xs js       = fst . runBState (elemsByIndsM js xs) $ indBase
 
-elemsByNotInds :: (F.Foldable t) => t a -> [Index] -> [a]
+elemsByNotInds :: [a] -> [Index] -> [a]
 elemsByNotInds xs js    = fst . runBState (elemsByNotIndsM js xs) $ indBase
 
-indsByElems :: (F.Foldable t, F.Foldable t1) =>
-               (a -> a -> Bool) -> t a -> t1 a -> [Index]
+indsByElems :: (a -> a -> Bool) -> [a] -> [a] -> [Index]
 indsByElems eq xs ks    = fst . runBState (indsByElemsM eq ks xs) $ indBase
 
 -- Some more specific "instances" of above functions.
-elemByInd :: (F.Foldable t) => t a -> Index -> [a]
+elemByInd :: [a] -> Index -> [a]
 elemByInd xs j      = elemsByInds xs [j]
 
-indsByElem :: (F.Foldable t) => (a -> a -> Bool) -> t a -> a -> [Index]
+indsByElem :: (a -> a -> Bool) -> [a] -> a -> [Index]
 indsByElem eq xs k  = indsByElems eq xs [k]
 
 -- Convert list of elements into list of corresponding indexes in "reference"
 -- list (preserving order of elements).
-elemsOrder :: (F.Foldable t, F.Foldable t1) =>
-              (a -> a -> Bool) -> t a -> t1 a -> [Index]
-elemsOrder eq       = F.concatMap . indsByElem eq
+elemsOrder :: (a -> a -> Bool) -> [a] -> [a] -> [Index]
+elemsOrder eq       = concatMap . indsByElem eq
 
 
 -- Sublists.
@@ -124,17 +119,17 @@ dropWhileEnd p      = foldr (\x z -> if null z && p x then [] else x : z) []
 -- Split list by list (separator is list of elements). Does not omit separator
 -- itself, just leave it as a separate element.  Note, that this function
 -- can't be implemented using Backward State monad.
-splitBy :: (F.Foldable t, Alternative f, Eq a) => [a] -> t a -> [f a]
+splitBy :: (Alternative f, Eq a) => [a] -> [a] -> [f a]
 splitBy ks          = let ks' = reverse ks
                       in  fst
                             . flip runState ks'
                             . flip runReaderT ks'
                             . splitByM
 
-splitByM :: (F.Foldable t, Alternative f, Eq a) =>
-            t a -> ReaderT [a] (State [a]) [f a]
+splitByM :: (Alternative f, Eq a) =>
+            [a] -> ReaderT [a] (State [a]) [f a]
 splitByM xs         = do
-                        (z1 : z2 : zs) <- F.foldrM fM [empty, empty] xs
+                        (z1 : z2 : zs) <- foldrM fM [empty, empty] xs
                         return ((z1 <|> z2) : zs)
   where
     -- z1 is "probably separator", z2 is column behind the separator.
@@ -156,9 +151,9 @@ splitByM xs         = do
           | x == k      = (pure x : (z1 <|> z2) : zs, ks)
           | otherwise   = (empty  : (pure x <|> z1 <|> z2) : zs, k : ks)
 
--- Apply user-defined function g to every element in input list. Then evaluate
--- State of its result, and if it's True, mappend new element (returned by g)
--- into head of accumulator, otherwise, just add it to the accumulator (list).
+-- Apply user-defined function g to every element in input list. Then if it
+-- returns True along with new element (monoid), mappend new element into head
+-- of accumulator, otherwise just add it to the accumulator list.
 foldrMerge :: (Monad m, Monoid b) => (a -> (m b, Bool)) -> [a] -> m [b]
 foldrMerge g        = foldrM (\(mx, p) zs -> mx >>= return . f p zs) [] . map g
   where
@@ -166,13 +161,6 @@ foldrMerge g        = foldrM (\(mx, p) zs -> mx >>= return . f p zs) [] . map g
     f p (z : zs) x'
       | p           = x' `mappend` z : zs
       | otherwise   = x' : z : zs
-
-g2 :: String -> (BState [String] [String], Bool)
-g2 xs             = (BState $ \(k : ks) -> (splitBy k xs, ks), 'a' `elem` xs)
-
-g2' :: String -> (BState [String] [String], Bool)
-g2' xs             = (BState $ \(k : ks) -> (splitBy k xs, ks), False)
-
 
 -- Instead of discarding longer tail (like zipWith do), add it to the result
 -- as is.
@@ -189,7 +177,7 @@ transp :: (a -> a -> Bool) -> [a] -> [a] -> [a]
 transp eq ks xs     = fst $ runBState (transpM eq xs) ks
 
 transpM :: (a -> a -> Bool) -> [a] -> BState [a] [a]
-transpM eq          = F.foldrM (\x -> BState . f x) []
+transpM eq          = foldrM (\x -> BState . f x) []
   where
     --f :: a -> [a] -> [a] -> ([a], [a])
     f _ _  []           = ([], [])
